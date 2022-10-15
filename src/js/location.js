b31a5b9bc0d6c3c42dff101632a28aa71a82331d
Michał Masiulanis <m.masiulanis@gmail.com>
2022-10-15 21:45:43 +0200

    modal + location
    nie perfect, ale dziś już nic nie wymyśle

--- src/js/location.js
+++ src/js/location.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-1]'),
-    closeModalBtn: document.querySelector('[data-modal-close-1]'),
-    modal: document.querySelector('[data-modal-1]'),
+    openModalBtn: document.querySelector('[data-modal-open-2]'),
+    closeModalBtn: document.querySelector('[data-modal-close-2]'),
+    modal: document.querySelector('[data-modal-2]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
