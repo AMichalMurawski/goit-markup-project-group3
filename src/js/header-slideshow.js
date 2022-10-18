aa74ae058ee8ab3633742b6c99ae2796ab874018
Rafał Gałecki <darthunter@gmail.com>
2022-10-18 20:49:15 +0200

    Imiona do slideshow
    plus tekst startowy i nowy after

--- src/js/header-slideshow.js
+++ src/js/header-slideshow.js
@@ -5,8 +5,9 @@
     "Everything we make with love and passion, that's why our icecreams are so delicious.",
     'We use only pure and natural ingredients. Milk from real cows. No digital. No virtual. No shit.',
     'However we also develop webpages, as you can see from this example.',
-    'Meet our dream team: Asia, Ola, Kamila, Michał, Kamil, Michamas, Grzegorz, Dawid, Rafał.',
+    'Meet our dream team: Oleńka, Dżołi, Kamila, Murek, Lesiu, Grzesiu, Michamas, Dawid, Rafał.',
     "If you want to have your own exceptional webpage, call us. We'll do the best.",
+    "Learn the secret of our success. Find out why everyone loves Icecream. It's a kind of magic!",
   ];
   let i = 0;
   starter.onclick = function () {
