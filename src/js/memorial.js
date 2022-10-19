7191bb3909df8e4d33b50a9b0993a5382e21d61a
Michał Murawski <109108678+AMichalMurawski@users.noreply.github.com>
2022-10-19 12:13:12 +0200

    Update memorial.js

--- src/js/memorial.js
+++ src/js/memorial.js
@@ -3,13 +3,12 @@
   const memoryBtn = document.querySelector('[js-memorial-btn]')
   const memoryDiv = document.createElement("div");
   const memoryData = `<img class="memory__ribbon" src="./images/memory-ribbon.png" alt="ribbon remembrance" /><div class="memory__robbie-coltrane"><p class="memory__quote">"So you could be watching it in 50 year's time, easily... I'll not be here, sadly... but Hagrid will, yes." - Robbie Coltrane</p><img src="./images/memory-robbie-coltrane.jpg" alt="Robbie Coltrane, lived from 1950 to 2022" /></div>`;
+  
   memoryDiv.classList.add('memory');
   memoryDiv.classList.add('memory__show');
   memoryDiv.innerHTML = memoryData;
+  
   const removeDiv = () => {memoryDiv.remove();};
-  const showMemory = () => {
-    
-  }
   
   memoryBtn.addEventListener('click', ()=>{
     memoryBody.prepend(memoryDiv);
